e3bba83e88de4fa9ef8de8187ae565e2553d08a2
Sameeksha-11 <gurrala.s@northeastern.edu>
2022-05-17 22:54:38 -0400

    Assignment - 0

--- src/main/java/edu/neu/coe/info6205/NewtonApproximation.java
+++ src/main/java/edu/neu/coe/info6205/NewtonApproximation.java
@@ -10,12 +10,12 @@ class NewtonApproximation {
         double x = 1.0;
         int left = 200;
         for (; left > 0; left--) {
-            final double y = Math.cos(x) - x;
+            final double y = 1/Math.cos(x) - x;
             if (Math.abs(y) < 1E-7) {
-                System.out.println("the solution to cos(x)=x is: " + x);
+                System.out.println("the solution to sec(x)=x is: " + x);
                 System.exit(0);
             }
-            x = x + y / (Math.sin(x) + 1);
+            x = x - y / (Math.tan(x)*(1/Math.cos(x)) - 1);
         }
     }
 }
